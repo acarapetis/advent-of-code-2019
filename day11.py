97c02b05c7490ea9b224722b764ab2824075376b
Anthony Carapetis <anthony.carapetis@gmail.com>
2019-12-12 22:09:15 +1100

    Day 11 solution

---
+++ day11.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+import sys
+from intcode import IntProc
+from collections import defaultdict
+
+code = sys.stdin.read()
+def run_robot(starting_color=0):
+    pos = (0,0)
+    facing = (0,1)
+    canvas = defaultdict(lambda: 0)
+    canvas[pos] = starting_color
+    cpu = IntProc(code)
+
+    def rotate(v):
+        nonlocal facing
+        if v:
+            facing = (facing[1], -facing[0])
+        else:
+            facing = (-facing[1], facing[0])
+    def move():
+        nonlocal pos
+        pos = (pos[0] + facing[0], pos[1] + facing[1])
+
+    while True:
+        try:
+            cpu.write(canvas[pos])
+            canvas[pos] = cpu.read()
+            rotate(cpu.read())
+            move()
+        except EOFError:
+            return canvas
+
+print(len(run_robot(0)))
+registration = run_robot(1)
+
+xmin = min(x[0] for x in registration.keys())
+ymin = min(x[1] for x in registration.keys())
+xmax = max(x[0] for x in registration.keys())
+ymax = max(x[1] for x in registration.keys())
+
+for y in range(ymax, ymin-1, -1):
+    for x in range(xmin, xmax+1):
+        print(registration[(x,y)] and '#' or '.', end='')
+    print()
